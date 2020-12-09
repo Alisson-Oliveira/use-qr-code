73a452bdf63ec7a9317285e0b0f677f1007b33e1
Alisson-Oliveira <alisskn.olisskn@gmail.com>
2020-12-09 13:44:55 -0300

    Check link.

--- mobile/src/pages/Tools.tsx
+++ mobile/src/pages/Tools.tsx
@@ -4,12 +4,33 @@ import { RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { GET_STORAGE, REMOVE_ALL, REMOVE_LINK } from '../config/storage';
+import { GET_LANGUAGE } from '../config/languages';
+import { tools as engTools } from '../languages/english';
+import { tools as porTools } from '../languages/portuguese';
+
+import Header from '../components/Header';
 
 export default function Tools() {
   const [links, setLinks] = useState<LinkProps[]>([]);
   const navigation = useNavigation();
+  const [languageScene, setLanguageScene] = useState<LanguageToolsProps>();
   
   useEffect(() => { 
+    GET_LANGUAGE()
+    .then(languageData => {
+      if (languageData) {
+        if (languageData === 'english') {
+          setLanguageScene(engTools);
+        } else {
+          setLanguageScene(porTools);
+        }
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      setLanguageScene(engTools);
+    });
+
     GET_STORAGE().then(response => {
       if (response !== undefined) {
         setLinks(response);
@@ -25,8 +46,8 @@ export default function Tools() {
     return navigation.navigate('About');
   }
 
-  function handleToContactMe() {
-    return navigation.navigate('ContactMe');
+  function handleToSettings() {
+    return navigation.navigate('Settings');
   }
 
   function handleRemoveAll() {
@@ -39,7 +60,7 @@ export default function Tools() {
         console.error(err);
         alert("Couldn't load page");
       });
-  };
+  }
 
   function handleShareLink(link: string) {
     try {
@@ -57,69 +78,79 @@ export default function Tools() {
     REMOVE_LINK(link);
   } 
 
+  function checkLink(link: string) {
+    return link.indexOf('http') > -1;
+  }
+
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.tools}>        
-        <View style={styles.containerButtons}>
-          <Text style={styles.title}>Tools</Text>
-          <RectButton style={styles.button} onPress={handleToScanQrCode}>
-            <Feather style={styles.icon} name="maximize" size={32} />
-            <Text style={styles.subTitle}>Scan QR Code</Text>
-          </RectButton>
-          <RectButton style={styles.button} onPress={handleToAbout}>
-            <Feather style={styles.icon} name="info" size={32} />
-            <Text style={styles.subTitle}>About</Text>
-          </RectButton>
-          <RectButton style={styles.button} onPress={() => alert('Em breve...')}>
-            <Feather style={styles.icon} name="settings" size={32} />
-            <Text style={styles.subTitle}>Settings</Text>
-          </RectButton>
-          <RectButton style={styles.button} onPress={handleToContactMe}>
-            <Feather style={styles.icon} name="mail" size={32} />
-            <Text style={styles.subTitle}>Contact Me</Text>
-          </RectButton>
-        </View>
-        <View style={styles.containerCleanAll}>
-          <Text style={styles.title}>Recently Opened</Text> 
-          <RectButton style={styles.containerButtonCleanAll} onPress={handleRemoveAll}>
-            <Text style={styles.titleCleanAll}>Clean All</Text>
-            <Feather name="trash-2" size={16} color='#FF4040' />
-          </RectButton>
-        </View>
-        <View style={styles.links}>
-          {
-            links.length !== 0 ? (
-              links.map((response, index) => (
-                <View key={index} style={styles.containerLink}> 
-                  <RectButton style={styles.link} onPress={() => handleAccessLink(response.link)}>
-                    <Feather style={styles.icon} name="link" size={27} />
-                    <Text style={styles.dominio}>{response.link}</Text>
-                  </RectButton>
-                  <RectButton onPress={() => handleShareLink(response.link)}>
-                    <Feather style={[styles.iconShareClose, { marginRight: 6 }]} name="share-2" size={20} />
-                  </RectButton>
-                  <RectButton onPress={() => handleRemoveLink(response.link)}>
-                    <Feather style={styles.iconShareClose} name="x" size={20} />
-                  </RectButton>
+    <>
+      <Header title='Use QR Code' />
+      <ScrollView style={styles.container}>
+        <View style={styles.tools}>        
+          <View>
+            <Text style={styles.title}>{languageScene?.tools}</Text>
+            <View style={{backgroundColor: '#FFFFFF', borderRadius: 12, padding: 12, marginVertical: 12, elevation: 1}}>
+              <RectButton style={styles.button} onPress={handleToScanQrCode}>
+                <Feather style={styles.icon} name="maximize" size={30} />
+                <Text style={styles.subTitle}>{languageScene?.scan}</Text>
+              </RectButton>
+              <RectButton style={styles.button} onPress={handleToAbout}>
+                <Feather style={styles.icon} name="info" size={30} />
+                <Text style={styles.subTitle}>{languageScene?.about}</Text>
+              </RectButton>
+              <RectButton style={styles.button} onPress={handleToSettings}>
+                <Feather style={styles.icon} name="settings" size={30} />
+                <Text style={styles.subTitle}>{languageScene?.settings}</Text>
+              </RectButton>
+            </View>
+          </View>
+          <View style={styles.containerCleanAll}>
+            <Text style={styles.title}>{languageScene?.recently}</Text> 
+            <RectButton style={styles.containerButtonCleanAll} onPress={handleRemoveAll}>
+              <Text style={styles.titleCleanAll}>{languageScene?.clean}</Text>
+              <Feather name="trash-2" size={16} color='#FF4040' />
+            </RectButton>
+          </View>
+          <View style={styles.links}>
+            {
+              links.length !== 0 ? (
+                links.map((response, index) => (
+                  <View key={index} style={styles.containerLink}> 
+                    <RectButton style={styles.link} onPress={() => handleAccessLink(response.link)}>
+                      {
+                        checkLink(response.link) ? 
+                          <Feather style={styles.icon} name="link" size={27} /> : 
+                          <Feather style={styles.icon} name="file-text" size={27} />
+                      }
+                      <Text style={styles.dominio}>{response.link}</Text>
+                    </RectButton>
+                    <RectButton onPress={() => handleShareLink(response.link)}>
+                      <Feather style={[styles.iconShareClose, { marginRight: 6 }]} name="share-2" size={20} />
+                    </RectButton>
+                    <RectButton onPress={() => handleRemoveLink(response.link)}>
+                      <Feather style={styles.iconShareClose} name="x" size={20} />
+                    </RectButton>
+                  </View>
+                ))
+              ) : (
+                <View style={styles.containerEmpty}>
+                  <Feather name="alert-triangle" size={20} color='#000000' />
+                  <Text>{languageScene?.empty}</Text>
                 </View>
-              ))
-            ) : (
-              <View style={styles.containerEmpty}>
-                <Feather name="alert-triangle" size={20} color='#000000' />
-                <Text>Has no link available :(</Text>
-              </View>
-            )
-          }
-        </View>
-      </View>  
-    </ScrollView>
+              )
+            }
+          </View>
+        </View>  
+      </ScrollView>
+    </>
+
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
+    backgroundColor: '#F2F2F2',
   },
 
   tools: {
@@ -127,16 +158,12 @@ const styles = StyleSheet.create({
     padding: 24,
   },
 
-  containerButtons: {
-    marginBottom: 12,
-  },
-
   title: {
     fontSize: 18,
   }, 
 
   button: {
-    height: 64,
+    height: 54,
     flexDirection: 'row',
     alignItems: 'center',
   },
@@ -175,7 +202,7 @@ const styles = StyleSheet.create({
   containerEmpty: {
     padding: 9,
     height: 64,
-    elevation: 3,
+    elevation: 1,
     marginTop: 12,
     borderRadius: 12,
     alignItems: 'center', 
